acb816012f158a20a01584b6f45376f0960ea564
202411010 <202411010@F1604-22>
2024-11-21 15:52:24 +0800

    Add project files.

---
+++ New Code - Github/New Code - Github.cpp
@@ -0,0 +1,16 @@
+// New Code - Github.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	num = 0;
+	do
+	{
+		num = num + 1;
+		cout << num << "...";
+	} while (num < 5);
+}
